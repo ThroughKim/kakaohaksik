7007ddd9648ca3d4b512276ef046aaebf6d276b0
KimSeulWoo <throughkim@KimSeulWooui-MacBook-Pro.local>
2016-06-29 17:51:06 +0900

    응답 메세지 수정

--- dguhaksik/views.py
+++ dguhaksik/views.py
@@ -8,8 +8,11 @@ def keyboard(request):
         'type' : 'buttons',
         'buttons' : ['상록원', '그루터기', '아리수', '기숙사식당', '교직원식당']})
 
+
 def answer(request):
 
     return JsonResponse({
-        "text" : "메뉴를 준비중입니다"
+        "message" : {
+            "text" : "메뉴를 준비중입니다."
+        }
     })
