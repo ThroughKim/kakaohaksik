0c775740ab58d272d28aa00b2c51bc6cdb15b1ca
KimSeulWoo <throughkim@KimSeulWooui-MacBook-Pro.local>
2016-06-29 17:31:05 +0900

    응답 기초

--- dguhaksik/views.py
+++ dguhaksik/views.py
@@ -7,3 +7,11 @@ def keyboard(request):
     return JsonResponse({
         'type' : 'buttons',
         'buttons' : ['상록원', '그루터기', '아리수', '기숙사식당', '교직원식당']})
+
+def answer(request):
+
+    return JsonResponse({
+        "message" : {
+            "text" : "메뉴를 준비중입니다"
+        }
+    })
--- kakao/urls.py
+++ kakao/urls.py
@@ -25,5 +25,6 @@ router = routers.DefaultRouter()
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^keyboard/', 'dguhaksik.views.keyboard')
+    url(r'^keyboard/', 'dguhaksik.views.keyboard'),
+    url(r'^message/', 'dguhaksik.views.answer'),
 ]
