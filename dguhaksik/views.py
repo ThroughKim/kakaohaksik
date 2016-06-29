c1fcf77d039fb8eb87b252aa32702ae80ba511fd
KimSeulWoo <throughkim@KimSeulWooui-MacBook-Pro.local>
2016-06-29 18:24:19 +0900

    메세지 수정

--- dguhaksik/views.py
+++ dguhaksik/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
 # Create your views here.
@@ -8,11 +8,16 @@ def keyboard(request):
         'type' : 'buttons',
         'buttons' : ['상록원', '그루터기', '아리수', '기숙사식당', '교직원식당']})
 
-
+@csrf_exempt
 def answer(request):
 
+    #cafeteria_name = request.POST.get("content")
+
     return JsonResponse({
-        "message" : {
-            "text" : "메뉴를 준비중입니다."
+        'message' : {
+            'text' : '메뉴를 준비중입니다.'
         }
     })
+
+
+#curl -XPOST 'https://127.0.0.1/message' -d '{"user_key": "encryptedUserKey", "type": "text", "content": "차량번호등록"}'
