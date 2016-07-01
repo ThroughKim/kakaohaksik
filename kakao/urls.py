711d9ad4ee81806dc9986302d814001f0fd77fce
KimSeulWoo <throughkim@KimSeulWooui-MacBook-Pro.local>
2016-07-01 14:26:00 +0900

    메세지 url수정

--- kakao/urls.py
+++ kakao/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^keyboard/', 'dguhaksik.views.keyboard'),
-    url(r'^message/', 'dguhaksik.views.answer'),
+    url(r'^message', 'dguhaksik.views.answer'),
     url(r'^friend', 'dguhaksik.views.friend'),
 ]
