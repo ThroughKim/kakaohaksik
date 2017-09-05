d746c7504d45f38a76a3eb3dc7463761f5787e53
SeulWoo Kim <sulewoo58@gmail.com>
2017-09-05 13:08:15 +0900

    응답프로세스 추가

--- pubgbot/views.py
+++ pubgbot/views.py
@@ -1,6 +1,6 @@
-from django.template.response import TemplateResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 
@@ -9,4 +9,20 @@ def keyboard(request):
     return JsonResponse({
         'type' : 'text',
         'content': '닉네임을 입력해주세요.'
+    })
+
+@csrf_exempt
+def answer(request):
+    json_str = ((request.body).decode('utf-8'))
+    received_json_data = json.loads(json_str)
+    nickname = received_json_data['content']
+
+    return JsonResponse({
+        'message': {
+            'text': nickname + "의 전적"
+        },
+        'keyboard': {
+            'type' : 'text',
+            'content': '닉네임을 입력해주세요.'
+        }
     })
