1ef8b67c2b9c94148008a040356c3435ea6a5bf7
SeulWoo Kim <sulewoo58@gmail.com>
2017-09-26 16:24:51 +0900

    remove 주석

--- pubgbot/views.py
+++ pubgbot/views.py
@@ -53,7 +53,6 @@ def answer(request):
         time_gap = timedelta(minutes=1)
         db_time = localtime(user_info.timestamp)
         if db_time < (now-time_gap):
-        # if db_time.date() == now.date() and db_time.time() < (now - time_gap).time() :
             # 시간 not_ok일 경우 api콜
             tasks.get_user(username)
             return JsonResponse({
