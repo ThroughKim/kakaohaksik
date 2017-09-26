b81c15c9a24141ea8338a34048d40c61e9f8e138
SeulWoo Kim <sulewoo58@gmail.com>
2017-09-26 18:39:19 +0900

    show more stats in admin

--- pubgbot/admin.py
+++ pubgbot/admin.py
@@ -8,13 +8,16 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'user_name', 'timestamp', 'season']
 
 class SoloAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user_name', 'timestamp', 'rounds_played', 'rating']
+    list_display = ['id', 'user_name', 'timestamp', 'rounds_played', 'rating', 'best_rank', 'win_ratio', 'top_10_ratio',
+                    'kill_death_ratio', 'kills_pg', 'damage_pg', 'round_most_kills']
 
 class DuoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user_name', 'timestamp', 'rounds_played', 'rating']
+    list_display = ['id', 'user_name', 'timestamp', 'rounds_played', 'rating', 'best_rank', 'win_ratio', 'top_10_ratio',
+                    'kill_death_ratio', 'kills_pg', 'damage_pg', 'round_most_kills']
 
 class SquadAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user_name', 'timestamp', 'rounds_played', 'rating']
+    list_display = ['id', 'user_name', 'timestamp', 'rounds_played', 'rating', 'best_rank', 'win_ratio', 'top_10_ratio',
+                    'kill_death_ratio', 'kills_pg', 'damage_pg', 'round_most_kills']
 
 admin.site.register(Log, LogAdmin)
 admin.site.register(Users, UserAdmin)
