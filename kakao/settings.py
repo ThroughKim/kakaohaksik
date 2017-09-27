5cedfe354f6ebcf8f1cab6963f36771fd92bf231
SeulWoo Kim <sulewoo58@gmail.com>
2017-09-27 10:23:47 +0900

    fix bug with time

--- kakao/settings.py
+++ kakao/settings.py
@@ -131,6 +131,28 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django':
+            {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+    },
+}
+
+
 import djcelery
 
 djcelery.setup_loader()
--- pubgbot/views.py
+++ pubgbot/views.py
@@ -1,11 +1,13 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from pubgbot.models import Users, SoloStats, DuoStats, SquadStats
-import json
 from datetime import datetime, timedelta
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.timezone import localtime
 import pubgbot.tasks as tasks
+import pytz
+import json
+
 
 
 def keyboard(request):
@@ -40,7 +42,8 @@ def answer(request):
         tasks.get_user(username)
         return JsonResponse({
             'message': {
-                'text': "전적 정보 업데이트 중입니다. 잠시 후 다시 시도해주세요. \n존재하지 않는 계정의 경우 업데이트되지 않습니다."
+                'text': "전적 정보 업데이트 중입니다. 잠시 후 다시 시도해주세요. \n"
+                        "존재하지 않는 계정의 경우 업데이트되지 않습니다."
             },
             'keyboard': {
                 'type': 'buttons',
@@ -49,51 +52,40 @@ def answer(request):
         })
     #유저명 있을 경우
     else:
-        now  = datetime.now()
-        time_gap = timedelta(minutes=1)
-        db_time = localtime(user_info.timestamp)
-        if db_time.date() == now.date() and db_time.time() < (now - time_gap).time() :
-            # 시간 not_ok일 경우 api콜
-            tasks.get_user(username)
-            return JsonResponse({
-                'message': {
-                    'text': "최신 전적 정보를 가져오는 중입니다. 잠시 후 다시 시도해주세요."
-                },
-                'keyboard': {
-                    'type': 'buttons',
-                    'buttons': [username, "다른계정"]
-                }
-            })
-        else:
-            solo_stats = SoloStats.objects.filter(user_name=username).last()
-            duo_stats = DuoStats.objects.filter(user_name=username).last()
-            squad_stats = SquadStats.objects.filter(user_name=username).last()
+        solo_stats = SoloStats.objects.filter(user_name=username).last()
+        duo_stats = DuoStats.objects.filter(user_name=username).last()
+        squad_stats = SquadStats.objects.filter(user_name=username).last()
 
-            solo_msg = make_msg("솔로", solo_stats)
-            duo_msg = make_msg("듀오", duo_stats)
-            squad_msg = make_msg("스쿼드", squad_stats)
+        solo_msg = make_msg("솔로", solo_stats)
+        duo_msg = make_msg("듀오", duo_stats)
+        squad_msg = make_msg("스쿼드", squad_stats)
 
-            sending_msg = username + "님의 " + user_info.season + " 시즌 전적 \n\n"
-            if username.lower() == 'godmori':
-                sending_msg = "⭐턱별회원 린치클럽 에이스 BJ ⭐갓갓갓모리⭐님의 " + user_info.season + " 시즌 전적 \n\n"
-            elif username.lower() == 'jrae3391':
-                sending_msg = "⭐핵고수 밀베왕 학교일진 강남조폭 여포갑 ⭐케챱도둑⭐님의 " + user_info.season + " 시즌 전적 \n\n"
-            sending_msg += solo_msg + duo_msg + squad_msg
+        sending_msg = username + "님의 " + user_info.season + " 시즌 전적 \n\n"
+        if username.lower() == 'godmori':
+            sending_msg = "⭐턱별회원 린치클럽 에이스 BJ ⭐갓갓갓모리⭐님의 " + user_info.season + " 시즌 전적 \n\n"
+        elif username.lower() == 'jrae3391':
+            sending_msg = "⭐핵고수 밀베왕 학교일진 강남조폭 여포갑 ⭐케챱도둑⭐님의 " + user_info.season + " 시즌 전적 \n\n"
+        sending_msg += solo_msg + duo_msg + squad_msg
 
-            return JsonResponse({
-                'message': {
-                    'text': sending_msg
-                },
-                'keyboard': {
-                    'type': 'buttons',
-                    'buttons': [username, "다른계정"]
-                }
-            })
+        return JsonResponse({
+            'message': {
+                'text': sending_msg
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': [username, "다른계정"]
+            }
+        })
 
 
 def search_user_info(username):
+    now = datetime.now(pytz.timezone('Asia/Seoul'))
+    time_gap = timedelta(minutes=1)
     try:
-        user_info = Users.objects.filter(user_name=username).last()
+        user_info = Users.objects.filter(
+            user_name=username,
+            timestamp__gte=now-time_gap
+        ).last()
     except ObjectDoesNotExist:
         user_info = None
 
