857daf2187182d081237f4a9d1c9b9c076405582
SeulWoo Kim <sulewoo58@gmail.com>
2017-10-18 09:24:18 +0900

    no update

--- pubgbot/views.py
+++ pubgbot/views.py
@@ -23,44 +23,18 @@ def answer(request):
     received_json_data = json.loads(json_str)
     username = received_json_data['content']
 
-    if username == "다른계정":
+    ### 임시버전~~~~
+    user_info = get_user_info_without_update(username)
+    if not user_info:
         return JsonResponse({
             'message': {
-                'text': "닉네임을 입력해주세요."
+                'text': "현재 PUBG Tracker가 동작하지 않아 새로운 전적을 업데이트할 수 없습니다."
             },
             'keyboard': {
                 'type': 'text',
                 'content': "닉네임을 입력해주세요."
             }
         })
-
-    #유저명 검색
-    user_info = search_user_info(username)
-    # 1분 내 저장된 유저가 없는 경우
-    if not user_info:
-        # 에러 유저인지 검사
-        if username_has_error(username):
-            return JsonResponse({
-                'message': {
-                    'text': username + "의 전적정보를 찾을 수 없습니다."
-                },
-                'keyboard': {
-                    'type': 'text',
-                    'content': "닉네임을 입력해주세요."
-                }
-            })
-        else:
-            tasks.get_user(username)
-            return JsonResponse({
-                'message': {
-                    'text': "전적 정보 업데이트 중입니다. 잠시 후 다시 시도해주세요."
-                },
-                'keyboard': {
-                    'type': 'buttons',
-                    'buttons': [username, "다른계정"]
-                }
-            })
-    #유저가 있을 경우
     else:
         solo_stats = SoloStats.objects.filter(user_name=username).last()
         duo_stats = DuoStats.objects.filter(user_name=username).last()
@@ -70,7 +44,8 @@ def answer(request):
         duo_msg = make_msg("듀오", duo_stats)
         squad_msg = make_msg("스쿼드", squad_stats)
 
-        sending_msg = username + "님의 " + user_info.season + " 시즌 전적 \n\n"
+        sending_msg = username + "님의 " + user_info.season + " 시즌 전적 \n "\
+                      +"현재 새로운 전적을 업데이트할 수 없습니다.\n\n"
         if username.lower() == 'godmori':
             sending_msg = "⭐턱별회원 린치클럽 에이스 스트리머 ⭐갓갓갓모리⭐님의 " + user_info.season + " 시즌 전적 \n\n"
         elif username.lower() == 'jrae3391':
@@ -91,6 +66,75 @@ def answer(request):
             }
         })
 
+    ### 원래버전~~~~~
+    # if username == "다른계정":
+    #     return JsonResponse({
+    #         'message': {
+    #             'text': "닉네임을 입력해주세요."
+    #         },
+    #         'keyboard': {
+    #             'type': 'text',
+    #             'content': "닉네임을 입력해주세요."
+    #         }
+    #     })
+    #
+    # #유저명 검색
+    # user_info = search_user_info(username)
+    # # 1분 내 저장된 유저가 없는 경우
+    # if not user_info:
+    #     # 에러 유저인지 검사
+    #     if username_has_error(username):
+    #         return JsonResponse({
+    #             'message': {
+    #                 'text': username + "의 전적정보를 찾을 수 없습니다."
+    #             },
+    #             'keyboard': {
+    #                 'type': 'text',
+    #                 'content': "닉네임을 입력해주세요."
+    #             }
+    #         })
+    #     else:
+    #         tasks.get_user(username)
+    #         return JsonResponse({
+    #             'message': {
+    #                 'text': "전적 정보 업데이트 중입니다. 잠시 후 다시 시도해주세요."
+    #             },
+    #             'keyboard': {
+    #                 'type': 'buttons',
+    #                 'buttons': [username, "다른계정"]
+    #             }
+    #         })
+    # #유저가 있을 경우
+    # else:
+    #     solo_stats = SoloStats.objects.filter(user_name=username).last()
+    #     duo_stats = DuoStats.objects.filter(user_name=username).last()
+    #     squad_stats = SquadStats.objects.filter(user_name=username).last()
+    #
+    #     solo_msg = make_msg("솔로", solo_stats)
+    #     duo_msg = make_msg("듀오", duo_stats)
+    #     squad_msg = make_msg("스쿼드", squad_stats)
+    #
+    #     sending_msg = username + "님의 " + user_info.season + " 시즌 전적 \n\n"
+    #     if username.lower() == 'godmori':
+    #         sending_msg = "⭐턱별회원 린치클럽 에이스 스트리머 ⭐갓갓갓모리⭐님의 " + user_info.season + " 시즌 전적 \n\n"
+    #     elif username.lower() == 'jrae3391':
+    #         sending_msg = "⭐핵고수 밀베왕 학교일진 강남조폭 여포갑 ⭐케챱도둑⭐님의 " + user_info.season + " 시즌 전적 \n\n"
+    #     elif username.lower() == 'virginbreakor':
+    #         sending_msg = "🐔개트롤 🐔짭무새 🐔호쌍새 🐔VirginbreaKOR 님의 " + user_info.season + " 시즌 전적 \n\n"
+    #     elif username.lower() == 'fixandy':
+    #         sending_msg = "🙏간디🙏 🏠기획부동산 마스터🏠 앤디초 Fixandy 님의 " + user_info.season + " 시즌 전적 \n\n"
+    #     sending_msg += solo_msg + duo_msg + squad_msg
+    #
+    #     return JsonResponse({
+    #         'message': {
+    #             'text': sending_msg
+    #         },
+    #         'keyboard': {
+    #             'type': 'buttons',
+    #             'buttons': [username, "다른계정"]
+    #         }
+    #     })
+
 
 def search_user_info(username):
     now = datetime.now(pytz.timezone('Asia/Seoul'))
@@ -99,6 +143,17 @@ def search_user_info(username):
         user_info = Users.objects.filter(
             user_name=username,
             timestamp__gte=now-time_gap
+        ).last()
+    except ObjectDoesNotExist:
+        user_info = None
+
+    return user_info
+
+
+def get_user_info_without_update(username):
+    try:
+        user_info = Users.objects.filter(
+            user_name=username,
         ).last()
     except ObjectDoesNotExist:
         user_info = None
