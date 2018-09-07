e23b5260545dac6afebc1b937dfbef157bb2f5f2
ThroughKim <sulewoo@naver.com>
2018-09-07 10:59:31 +0900

    add try in save db

--- dcbg/views.py
+++ dcbg/views.py
@@ -15,12 +15,20 @@ def crawl(request):
     with open(file_path, encoding='utf-8') as f:
         reader = csv.reader(f)
         next(reader, None)      #Skip header
+        cnt = 0
         for row in reader:
-            _, created = Chat.objects.get_or_create(
-                sender = row[1],
-                content = row[2],
-                time = dateutil.parser.parse(row[0])
-            )
+            try:
+                _, created = Chat.objects.get_or_create(
+                    sender = row[1],
+                    content = row[2],
+                    time = dateutil.parser.parse(row[0])
+                )
+            except:
+                print("ERROR")
+                print(row)
+            finally:
+                cnt = cnt + 1
+                print(str(cnt) + "개 완료")
 
     return JsonResponse({
         'crawl': 'done'
